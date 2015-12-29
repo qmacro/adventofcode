385e7900a856a6f233354b7fdfb7290e604d802f
DJ Adams <dj.adams@pobox.com>
2015-12-29 18:28:23 +0000

    remove extraneous solns for day01

--- src/adventofcode/day01.clj
+++ src/adventofcode/day01.clj
@@ -7,14 +7,6 @@
 ;; Seq of 1s and -1s representing the (s and )s
 (def invals (map {\( 1 \) -1} in))
 
-;; Part 1
-(reduce + invals)
-    
-;; Part 2
-(+ 1 ;; start counting at 1
-   1 ;; if we take away all the initial +ves, the next one is basement
-  (count (take-while pos? (reductions + invals))))
-
 (defn solve
   "..."
   [& args]
