ede00487613c1126bbc3bb925f176aa1a00cbfb7
DJ Adams <dj.adams@pobox.com>
2016-01-07 14:02:29 +0000

    solution to Day 18 part 1

--- src/adventofcode/day18.clj
+++ src/adventofcode/day18.clj
@@ -2,7 +2,7 @@
   (:require [clojure.string :as str]))
 
 (def input
-  (->> "resources/day18test"
+  (->> "resources/day18input"
        slurp
        str/split-lines))
 
@@ -12,22 +12,31 @@
   board."
   (vec (map vec input)))
 
-(def rowsize
+(defn rowsize
   "How many rows the board has."
+  [board]
   (count board))
 
-(def colsize
+(defn colsize
   "How many cols the board has."
+  [board]
   (count (first board)))
+
+(def on \#)
+(def off \.)
 
 (defn on?
   [cell]
-  (= cell \#))
+  (= cell on))
 
-(def coords
+(def off? (complement on?))
+
+
+(defn coords
   "Create seq of all the coordinates for the board."
-  (for [row (range rowsize)
-        col (range colsize)]
+  [board]
+  (for [row (range (rowsize board))
+        col (range (colsize board))]
     [row col]))
 
 (def rel-nbr-coords
@@ -42,33 +51,66 @@
 (defn cellstate
   "Return the state of a cell in the board,
   at the given row/col coord."
-  [[r c]]
+  [[r c] board]
   ((board r) c))
 
 (defn out-of-bounds?
   "Whether a given cell is out of bounds
   with respect to the board edges."
-  [[r c]]
+  [[r c] board]
   (or (< r 0)
-      (>= r rowsize)
+      (>= r (rowsize board))
       (< c 0)
-      (>= c colsize)))
+      (>= c (colsize board))))
 
 (defn neighbours
   "Returns the coords of the neighbours of a given cell,
   excluding the cell itself and any neighbours that are
   out of bounds."
-  [[r c]]
+  [[r c] board]
   (->> (map #(map + [r c] %) rel-nbr-coords)
        (filter #(not= [r c] %))
-       (filter #(not (out-of-bounds? %)))))
+       (filter #(not (out-of-bounds? % board)))))
 
 (defn neighbours-on
   "Returns how many neighbours of a given cell are on."
-  [[r c]]
-  (->> (neighbours [r c])
-       (map cellstate)
+  [[r c] board]
+  (->> (neighbours [r c] board)
+       (map #(cellstate % board))
        (filter #(on? %))
        count))
 
+(defn toggle
+  "Toggles a light."
+  [light]
+  (case light
+    on off
+    off on))
+
+(defn switch
+  "Switches (or not) a light for the next state iteration,
+  according to its current state, and how many of its 
+  neighbours are on."
+  [light n]
+  (if (or (and (off? light) (= 3 n))
+          (and (on? light) (and (not= 2 n) (not= 3 n))))
+    (if (on? light) off on)
+    light))
+
+(defn re-form
+  [flatboard c]
+  (vec (map vec (partition c flatboard))))
+
+(defn transform
+  "Transform board n steps."
+  [board n]
+  (loop
+    [b board
+     i n]
+    (if (zero? i)
+      b
+      (let [nbrs-on (->> (coords b) (map #(neighbours-on % b)))]
+        (recur (re-form (map switch (flatten b) nbrs-on) (colsize b)) (dec i))))))
+
+(count (filter on? (flatten (transform board 100))))
 
